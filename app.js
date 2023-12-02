ab45deff43ab5bbaa4ccb2e022cc034aee9f16dd
krati goyal <kratigoyalind@gmail.com>
2023-12-03 00:26:50 +0530

    js complete

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const addBtn = document.querySelector("#addBtn")
-const main = document.querySelector("main")
+const main = document.querySelector("#main")
+
+const saveNotes = () => {
+    const notes = document.querySelectorAll(".note textarea");
+    const data = [];
+    notes.forEach(
+        (note) => {
+            data.push(note.value)
+        }
+    )
+    if(data.length == 0){
+        localStorage.removeItem("notes")
+    } else{
+        localStorage.setItem("notes", JSON.stringify(data))
+    }
+}
 
 addBtn.addEventListener(
     "click",
@@ -8,17 +23,57 @@ addBtn.addEventListener(
     }
 )
 
-const addNote = () => {
+//<div class="note">
+//             <div class="tool">
+//                 <i class="fa-solid fa-floppy-disk"></i>
+//                 <i class="fa-solid fa-trash"></i>
+//             </div>
+//             <textarea></textarea>
+//  </div>
+
+
+const addNote = (text = "") => {
     const note = document.createElement("div");
-    note.classList.add("note");
+    note.classList.add("note")
     note.innerHTML = `
     <div class="tool">
-        <i class="fa-solid fa-floppy-disk"></i>
-        <i class="fa-solid fa-trash"></i>
+        <i class="save fa-solid fa-floppy-disk"></i>
+        <i class="trash fa-solid fa-trash"></i>
     </div>
-    <textarea></textarea>
+    <textarea>${text}</textarea>
     `;
 
-    main.appendChild(note);
+    note.querySelector(".trash").addEventListener(
+        "click",
+        function() {
+            note.remove()
+            saveNotes()
+        }
+    )
 
+    note.querySelector(".save").addEventListener(
+        "click",
+        function(){
+            saveNotes()
+        }
+    )
+
+    main.appendChild(note);
+    saveNotes()
 }
+
+(
+    function(){
+        const lsNotes = JSON.parse(localStorage.getItem("notes"));
+        lsNotes.forEach(
+                (lsNote) => {
+                    addNote(lsNote)
+                }
+            )
+            if(lsNotes.length == 0) {
+                localStorage.removeItem("notes")
+            } else {
+                addNote();
+            }
+        }
+)()
